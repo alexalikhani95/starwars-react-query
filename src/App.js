0817ae5b9ee23fddf226480b953c006977241220
Alex Alikhani <alikhanialex@gmail.com>
2022-06-11 15:51:56 +0100

    Importing basic planets and people components and rendering them based on page state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
+import People from "./components/People";
+import Planets from "./components/Planets";
 
 function App() {
+  const [page, setPage] = useState("planets");
   return (
     <div className="App">
       <h1>Star Wars Information</h1>
       <Navbar />
-      <div className="content"></div>
+      <div className="content">{page === "planets" ? <Planets /> : <People />}</div>
     </div>
   );
 }
