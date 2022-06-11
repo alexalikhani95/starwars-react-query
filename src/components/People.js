02b392518f9ffe7839e238db6b59090e0401ea69
Alex Alikhani <alikhanialex@gmail.com>
2022-06-11 20:42:45 +0100

    Adding a Person component and using it to display people data in the People component

--- src/components/People.js
+++ src/components/People.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { useQuery } from "react-query";
+import Person from "./Person";
+
+const fetchPeople = async () => {
+  const response = await fetch("https://swapi.dev/api/people/");
+  return response.json();
+};
 
 const People = () => {
-  return <div>People</div>;
+  // 'People' is the key for the Query, second argument is an async function to grab the data
+  const { data, status } = useQuery("People", fetchPeople);
+  console.log(data);
+  return (
+    <div>
+      <h2>People</h2>
+      {/* // status of query e.g loading */}
+      {status === "loading" && <div>Loading data...</div>}
+      {status === "error" && <div>Error fetching data</div>}
+      {status === "success" && (
+        <div>
+          {data.results.map((person) => (
+            <Person person={person} key={person.name} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default People;
