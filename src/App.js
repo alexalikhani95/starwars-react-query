f087ed80374cdecf1c9c28b65902588266b1b069
Alex Alikhani <alikhanialex@gmail.com>
2022-06-11 16:16:15 +0100

    Using react query to fetch the list of planets and display loading/error status

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import People from "./components/People";
 import Planets from "./components/Planets";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   const [page, setPage] = useState("planets");
   return (
-    <div className="App">
-      <h1>Star Wars Information</h1>
-      <Navbar setPage={setPage} />
-      <div className="content">{page === "planets" ? <Planets /> : <People />}</div>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Star Wars Information</h1>
+        <Navbar setPage={setPage} />
+        <div className="content">{page === "planets" ? <Planets /> : <People />}</div>
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { useQuery } from "react-query";
+
+const fetchPlanets = async () => {
+  const response = await fetch("https://swapi.dev/api/planets/");
+  return response.json();
+};
 
 const Planets = () => {
-  return <div>Planets</div>;
+  // 'Planets' is the key for the Query, second argument is an async function to grab the data
+  const { data, status } = useQuery("Planets", fetchPlanets);
+  console.log(data);
+  return (
+    <div>
+      <h2>Planets</h2>
+      {/* // status of query e.g loading */}
+      <p>{status}</p>
+    </div>
+  );
 };
 
 export default Planets;
