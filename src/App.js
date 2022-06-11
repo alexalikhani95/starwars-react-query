6e0545c8cacf785a37eb1e9b6c8b3a426e569f2a
Alex Alikhani <alikhanialex@gmail.com>
2022-06-11 15:49:11 +0100

    Importing basic navbar component into app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Star Wars Information</h1>
+      <Navbar />
+      <div className="content"></div>
+    </div>
+  );
 }
 
 export default App;
